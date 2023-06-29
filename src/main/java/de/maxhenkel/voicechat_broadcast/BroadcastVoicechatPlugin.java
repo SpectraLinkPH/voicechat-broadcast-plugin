75af5a4937875fa40c7c29ee6ae615d7e4930ece
SpectraLink <120117150+SpectraLinkPH@users.noreply.github.com>
2023-06-29 21:08:17 +0800

    Update BroadcastVoicechatPlugin.java

--- src/main/java/de/maxhenkel/voicechat_broadcast/BroadcastVoicechatPlugin.java
+++ src/main/java/de/maxhenkel/voicechat_broadcast/BroadcastVoicechatPlugin.java
@@ -4,16 +4,9 @@ import de.maxhenkel.voicechat.api.*;
 import de.maxhenkel.voicechat.api.events.EventRegistration;
 import de.maxhenkel.voicechat.api.events.MicrophonePacketEvent;
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.permissions.Permission;
 import org.bukkit.permissions.PermissionDefault;
-
-import java.util.HashSet;
-import java.util.Set;
-import java.util.UUID;
 
 public class BroadcastVoicechatPlugin implements VoicechatPlugin {
 
@@ -21,12 +14,6 @@ public class BroadcastVoicechatPlugin implements VoicechatPlugin {
      * Only OPs have the broadcast permission by default
      */
     public static Permission BROADCAST_PERMISSION = new Permission("voicechat_broadcast.broadcast", PermissionDefault.OP);
-
-    private Set<UUID> mutedPlayers;
-
-    public BroadcastVoicechatPlugin() {
-        this.mutedPlayers = new HashSet<>();
-    }
 
     /**
      * @return the unique ID for this voice chat plugin
@@ -96,8 +83,8 @@ public class BroadcastVoicechatPlugin implements VoicechatPlugin {
 
         // Iterating over every player on the server
         for (Player onlinePlayer : Bukkit.getServer().getOnlinePlayers()) {
-            // Don't send the audio to the player that is broadcasting or to muted players
-            if (onlinePlayer.getUniqueId().equals(player.getUniqueId()) || isPlayerMuted(onlinePlayer.getUniqueId())) {
+            // Don't send the audio to the player that is broadcasting
+            if (onlinePlayer.getUniqueId().equals(player.getUniqueId())) {
                 continue;
             }
             VoicechatConnection connection = api.getConnectionOf(onlinePlayer.getUniqueId());
@@ -108,67 +95,6 @@ public class BroadcastVoicechatPlugin implements VoicechatPlugin {
             // Send a static audio packet of the microphone data to the connection of each player
             api.sendStaticSoundPacketTo(connection, event.getPacket().toStaticSoundPacket());
         }
-    }
-
-    /**
-     * Check if a player is muted
-     *
-     * @param playerUUID the UUID of the player to check
-     * @return true if the player is muted, false otherwise
-     */
-    public boolean isPlayerMuted(UUID playerUUID) {
-        return mutedPlayers.contains(playerUUID);
-    }
-
-    /**
-     * Mute a player
-     *
-     * @param playerUUID the UUID of the player to mute
-     */
-    public void mutePlayer(UUID playerUUID) {
-        mutedPlayers.add(playerUUID);
-    }
-
-    /**
-     * Unmute a player
-     *
-     * @param playerUUID the UUID of the player to unmute
-     */
-    public void unmutePlayer(UUID playerUUID) {
-        mutedPlayers.remove(playerUUID);
-    }
-    
-    /**
-     * Handle the command to mute/unmute the broadcast
-     *
-     * @param sender the command sender
-     * @param command the command object
-     * @param label the command label
-     * @param args the command arguments
-     * @return true if the command was handled, false otherwise
-     */
-    public boolean handleCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (!(sender instanceof Player)) {
-            sender.sendMessage(ChatColor.RED + "Only players can use this command.");
-            return true;
-        }
-
-        Player player = (Player) sender;
-
-        if (!player.hasPermission(BROADCAST_PERMISSION)) {
-            player.sendMessage(ChatColor.RED + "You don't have permission to use this command.");
-            return true;
-        }
-
-        if (isPlayerMuted(player.getUniqueId())) {
-            unmutePlayer(player.getUniqueId());
-            player.sendMessage(ChatColor.GREEN + "Broadcast unmuted.");
-        } else {
-            mutePlayer(player.getUniqueId());
-            player.sendMessage(ChatColor.GREEN + "Broadcast muted.");
-        }
-
-        return true;
     }
 
 }
