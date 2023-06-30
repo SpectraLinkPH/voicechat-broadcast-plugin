84e9445484cdcc309b1e7cd3721989a2ee9ccc4e
SpectraLink <120117150+SpectraLinkPH@users.noreply.github.com>
2023-06-30 18:37:39 +0800

    asd

--- src/main/java/de/maxhenkel/voicechat_broadcast/GroupCommand.java
+++ src/main/java/de/maxhenkel/voicechat_broadcast/GroupCommand.java
@@ -7,7 +7,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 public class GroupCommand implements CommandExecutor {
-
+    
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (!(sender instanceof Player)) {
@@ -25,20 +25,6 @@ public class GroupCommand implements CommandExecutor {
         String name = args[0];
         String password = args.length >= 2 ? args[1] : null;
         boolean persistent = args.length >= 3 && Boolean.parseBoolean(args[2]);
-
-        VoicechatApi voicechatApi = VoicechatApi.getInstance();
-
-        if (voicechatApi == null) {
-            player.sendMessage("Error: Voicechat API not available.");
-            return true;
-        }
-
-        VoicechatServerApi voicechatServerApi = voicechatApi.getServerApi();
-
-        if (voicechatServerApi == null) {
-            player.sendMessage("Error: Voicechat server API not available.");
-            return true;
-        }
 
         Group.Builder groupBuilder = voicechatServerApi.groupBuilder()
                 .setName(name)
