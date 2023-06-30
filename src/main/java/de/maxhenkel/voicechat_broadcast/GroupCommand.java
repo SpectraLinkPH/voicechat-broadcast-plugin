2ba9c23faf1dc269c70fde49fd4d4d0a39b8d83b
SpectraLink <120117150+SpectraLinkPH@users.noreply.github.com>
2023-06-30 18:39:17 +0800

    asd

--- src/main/java/de/maxhenkel/voicechat_broadcast/GroupCommand.java
+++ src/main/java/de/maxhenkel/voicechat_broadcast/GroupCommand.java
@@ -1,13 +1,12 @@
 package de.maxhenkel.voicechat_broadcast;
 
-import de.maxhenkel.voicechat.api.*;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 public class GroupCommand implements CommandExecutor {
-    
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (!(sender instanceof Player)) {
